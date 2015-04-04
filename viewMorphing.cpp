64c0afbdf32bcd58fbb210582a007ecae476d98d
Murat Ambarkutuk <m.ambarkutuk@gmail.com>
2015-04-04 11:51:20 -0400

    Test commit: Win 8.1; Newark, DE

--- viewMorphing.cpp
+++ viewMorphing.cpp
@@ -7,17 +7,16 @@
 //============================================================================
 
 #include <iostream>
-#include "cv.h"
+
 #include "opencv2/core/core.hpp"
 #include "opencv2/opencv.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/features2d/features2d.hpp"
 #include "opencv2/flann/flann.hpp"
-#include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
-#include "opencv2/nonfree/features2d.hpp"
 #include "opencv2/calib3d/calib3d.hpp"
 #include "opencv2/legacy/legacy.hpp"
+
 #include "libViewMorphing.h"
 
 using namespace std;
